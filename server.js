e9d60071636932b2b0ae7f22ae2cb58433e6665a
Shannon Draper <shannon@roadmunk.com>
2018-09-08 11:29:32 -0400

    deleted unused packages

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const http         = require('http');
 const express      = require('express');
 const path         = require('path');
 const favicon      = require('static-favicon');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
 const less         = require('less-middleware');
-const clc          = require('cli-color');
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -15,25 +11,15 @@ const port = process.env.PORT || 3030;
 const app = express();
 
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-logger.token('colored-status', function(req, res) {
-	if (res.statusCode < 300) return clc.green(res.statusCode);
-	if (res.statusCode < 400) return clc.cyan(res.statusCode);
-	if (res.statusCode < 500) return clc.red(res.statusCode);
-	return clc.redBright(res.statusCode);
-});
-app.use(logger(':date[iso] :remote-addr :response-time :colored-status :method :url'));
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false }));
-app.use(cookieParser());
-
 app.use(less(path.join(__dirname, 'public'), { force : env == 'development' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'public', 'main.html'));
+	res.sendFile(path.join(__dirname, 'public', './views/main.html'));
 });
 http.createServer(app).listen(port);
 
