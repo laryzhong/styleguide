8399fbceddbece859d2ec79e71a0d642660ae3ef
Shannon Draper <shannon@roadmunk.com>
2018-09-10 09:21:35 -0400

    set up ejs as view engine

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http         = require('http');
 const express      = require('express');
 const path         = require('path');
-const favicon      = require('static-favicon');
 const less         = require('less-middleware');
 
 const env = process.env.NODE_ENV || 'development';
@@ -10,17 +9,17 @@ const port = process.env.PORT || 3030;
 
 const app = express();
 
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(less(path.join(__dirname, 'public'), { force : env == 'development' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'public', './views/main.html'));
+	res.render('main');
 });
+
 http.createServer(app).listen(port);
 
 console.log(`Listening on http://localhost:${port}`);
