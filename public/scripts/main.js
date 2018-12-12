4fd6c658db96a18bd65b707620966afcf3359af1
Shannon Draper <shannon@roadmunk.com>
2018-12-12 12:35:37 -0500

    added js to toggle aside open and closed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,18 @@
-document.getElementById('expand').addEventListener('click', function (e) {
-    e.preventDefault();
-    const componentDropdown = document.getElementById('dropdown');
-    componentDropdown.classList.toggle('hide');
+// expand collapsed options in navigation
+document.getElementById('expand').addEventListener('click', e => {
+	e.preventDefault();
+	const componentDropdown = document.getElementById('dropdown');
+	componentDropdown.classList.toggle('hide');
+});
+
+// collapse aside off screen
+document.getElementById('nav-toggle').addEventListener('click', () => {
+    // e.preventDefault();
+	const aside = document.getElementsByClassName('aside');
+	const content = document.getElementsByClassName('content');
+	const navToggle = document.getElementById('nav-toggle');
+
+	aside[0].classList.toggle('nav-closed');
+	content[0].classList.toggle('nav-closed');
+	navToggle.classList.toggle('nav-closed');
 });
