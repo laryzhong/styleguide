b3f43d69b75d52d27b8d453a8ec7be3e79cc8712
Shannon Draper <shannon@roadmunk.com>
2018-12-17 10:46:09 -0500

    working to get copy to clipboard function

--- public/scripts/color-palette.js
+++ public/scripts/color-palette.js
@@ -1,1 +1,45 @@
-console.log('this is the color-palette page');
+
+// toggle hex vs rgb code on colour palette page
+const buttonList = document.querySelectorAll('button');
+const hexButton  = buttonList[0];
+const rgbButton  = buttonList[1];
+const colorRgb   = document.querySelectorAll('.color-rgb');
+const colorHex   = document.querySelectorAll('.color-hex');
+
+hexButton.addEventListener('click', () => {
+	hexButton.classList.remove('selected');
+	rgbButton.classList.remove('selected');
+	hexButton.classList.add('selected');
+
+	i = 0,
+	rgbLength = colorRgb.length;
+
+	for (i; i < rgbLength; i++) {
+		colorRgb[i].style.display = 'none';
+		colorHex[i].style.display = 'block';
+	}
+});
+rgbButton.addEventListener('click', () => {
+	rgbButton.classList.remove('selected');
+	hexButton.classList.remove('selected');
+	rgbButton.classList.add('selected');
+
+	i = 0,
+	hexLength = colorHex.length;
+
+	for (i; i < hexLength; i++) {
+		colorHex[i].style.display = 'none';
+		colorRgb[i].style.display = 'block';
+	}
+});
+
+// copy hex or rgb value to clipboard
+const copyToClipboard = str => {
+	const el = document.createElement('textarea');
+	el.value = str;
+	document.body.appendChild(el);
+	el.select();
+	document.execCommand('copy');
+	console.log('copied!');
+	document.body.removeChild(el);
+};
